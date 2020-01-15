2320f7cc6877374da22c856b666d25ad8b7fe168
Cristobal Heis <cristobal.heiss.j@gmail.com>
2020-01-15 08:59:26 +0100

    fixed model tests

--- src/model.test.js
+++ src/model.test.js
@@ -2,18 +2,18 @@ const initialState = require('./model');
 
 test('should have property frontMessage', () => {
   const expected = true;
-  const received = initialState.hasOwnProperty('frontMessage');
+  const received = initialState.flashCards[0].hasOwnProperty('frontMessage');
   expect(received).toBe(expected);
 });
 
 test('should have property backMessage', () => {
   const expected = true;
-  const received = initialState.hasOwnProperty('backMessage');
+  const received = initialState.flashCards[0].hasOwnProperty('backMessage');
   expect(received).toBe(expected);
 });
 
 test('should have property isCurrentDisplayFront', () => {
   const expected = true;
-  const received = initialState.hasOwnProperty('isCurrentDisplayFront');
+  const received = initialState.flashCards[0].hasOwnProperty('isCurrentDisplayFront');
   expect(received).toBe(expected);
 });
